707320c20696bbefd553d4a7930e190b2de223fc
Centri3 <114838443+Centri3@users.noreply.github.com>
2023-02-16 21:25:45 -0600

    more work on headers

--- crates/starb/src/exe/headers.rs
+++ crates/starb/src/exe/headers.rs
@@ -1,21 +1,14 @@
+// TODO: Light documentation
+
+use crate::exe::EXE;
 use eyre::Result;
 use hashbrown::HashMap;
 use once_cell::sync::OnceCell;
 use std::fmt;
 use std::ops::Range;
 
-/// Internal function to reduce code repetition. Allows getting any of
-/// `HEADER`'s fields which are wrapped in `OnceCell<T>`.
-#[inline(always)]
-#[instrument(skip(value), level = "trace")]
-fn __get_initialized<T: fmt::Debug>(value: &OnceCell<T>) -> Result<&T> {
-    value
-        .get()
-        .ok_or(eyre!("`HEADERS` was uninitialized, please call `.init()`"))
-}
-
 /// Type definition for `IMAGE_DATA_DIRECTORY`.
-pub type NtDataDirectory = Vec<Range<usize>>;
+pub type NtDataDirectory = [Range<usize>; 16usize];
 /// Type definition for `IMAGE_SECTION_HEADER`.
 pub type NtImageSections = HashMap<String, Range<usize>>;
 
@@ -23,12 +16,16 @@ pub type NtImageSections = HashMap<String, Range<usize>>;
 pub static HEADERS: NtImage = NtImage::__define();
 
 #[derive(Debug, Default)]
+#[non_exhaustive]
 pub struct NtImage {
     optional: OnceCell<NtOptional>,
     sections: OnceCell<NtImageSections>,
 }
 
 impl NtImage {
+    // TODO: We should read sections/directories/whatever using constants defined
+    // here, as it's just easier to read
+
     /// Internal function to define `HEADERS`.
     #[inline(always)]
     const fn __define() -> Self {
@@ -38,32 +35,116 @@ impl NtImage {
         }
     }
 
+    /// Internal function to reduce code repetition. Allows getting any of
+    /// `HEADERS`' fields which are wrapped in `OnceCell<T>`.
+    #[inline(always)]
+    #[instrument(skip(value), level = "trace")]
+    fn __get_initialized<T: fmt::Debug>(value: &OnceCell<T>) -> Result<&T> {
+        value
+            .get()
+            .ok_or(eyre!("`HEADERS` was uninitialized, please call `.init()`"))
+    }
+
+    #[inline(always)]
+    #[instrument(skip(self))]
+    fn __get_optional(&self) -> Result<NtOptional> {
+        let entry_point = EXE.read_to::<u32>(0x198usize)? as usize;
+        let directory = self.__get_directory()?;
+
+        Ok(NtOptional::new(entry_point, directory))
+    }
+
+    #[inline(always)]
+    #[instrument(skip(self))]
+    fn __get_directory(&self) -> Result<NtDataDirectory> {
+        trace!("Getting data directory");
+
+        let mut directory = NtDataDirectory::default();
+
+        // FIXME: Is this really redunant? Or just bad design on my part?
+        // FIXME: Entries can be (NULL, NULL). We should catch this
+        #[allow(clippy::redundant_clone)]
+        for (i, _) in directory.clone().iter().enumerate() {
+            let start = EXE.read_to::<u32>(0x1f8usize + i * 0x8usize)? as usize;
+            let range = start..start + EXE.read_to::<u32>(0x1fcusize + i * 0x8usize)? as usize;
+
+            directory[i] = range;
+        }
+
+        // TODO: Remove later
+        info!("{:x?}", directory);
+
+        Ok(directory)
+    }
+
+    // TODO: This should be refactored. Kinda ugly
+    #[inline(always)]
+    #[instrument(skip(self))]
+    fn __get_sections(&self) -> Result<NtImageSections> {
+        trace!("Getting sections");
+
+        let mut sections = NtImageSections::with_capacity(EXE.read_to::<u16>(0x176usize)? as _);
+
+        for i in 0usize..sections.capacity() {
+            let base = 0x278usize + i * 0x28usize;
+
+            // Read name of section
+            let name = EXE.read_to_string(base, Some(8usize))?;
+
+            // Read range of section
+            let start = EXE.read_to::<u32>(base + 0x14usize)? as usize;
+            let range = start..start + EXE.read_to::<u32>(base + 0x10usize)? as usize;
+
+            info!(section = name, ?range, "Found section");
+
+            // Insert name of section and range of section
+            sections.insert(name, range);
+        }
+
+        Ok(sections)
+    }
+
     #[inline]
     #[instrument(skip(self))]
     pub fn init(&self) -> Result<()> {
+        info!("Initializing `HEADERS`");
+
+        self.__get_optional()?;
+
+        self.__get_sections()?;
+
         todo!();
     }
 
     #[inline]
     #[instrument(skip(self))]
     pub fn optional(&self) -> Result<&NtOptional> {
-        __get_initialized(&self.optional)
+        Self::__get_initialized(&self.optional)
     }
 
     #[inline]
     #[instrument(skip(self))]
     pub fn sections(&self) -> Result<&NtImageSections> {
-        __get_initialized(&self.sections)
+        Self::__get_initialized(&self.sections)
     }
 }
 
 #[derive(Debug, Default)]
+#[non_exhaustive]
 pub struct NtOptional {
     entry_point: usize,
     directory: NtDataDirectory,
 }
 
 impl NtOptional {
+    #[inline]
+    pub fn new(entry_point: usize, directory: NtDataDirectory) -> Self {
+        Self {
+            entry_point,
+            directory,
+        }
+    }
+
     #[inline]
     #[instrument(skip(self))]
     pub fn entry_point(&self) -> usize {
