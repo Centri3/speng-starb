790fff8b42341dd2be92428d3f1526f09d606be4
Centri3 <114838443+Centri3@users.noreply.github.com>
2023-02-17 21:50:18 -0600

    headers 2.0.0.0.0.0.0.0.0.0.0.0.0

--- crates/starb/src/exe/headers.rs
+++ crates/starb/src/exe/headers.rs
@@ -24,8 +24,24 @@ pub struct NtImage {
 }
 
 impl NtImage {
-    // TODO: We should read sections/directories/whatever using constants defined
-    // here, as it's just easier to read
+    /// Signature (or e_magic) of DOS headers
+    const MZ_SIGNATURE: usize = 0x0usize;
+    /// Signature of NT headers
+    const NT_SIGNATURE: usize = 0x170usize;
+    /// Entry point of program
+    const NT_ENTRY_POINT: usize = 0x198usize;
+    /// Length of each entry in `IMAGE_DATA_DIRECTORY`
+    const NT_DIRECTORY_ENTRY_LEN: usize = 0x8usize;
+    /// Where the data directory starts in the exe
+    const NT_DIRECTORY: usize = 0x1f8usize;
+    /// How many entries the data directory can contain
+    const NT_DIRECTORY_NUM: usize = 0x10usize;
+    /// Length of each section in `IMAGE_SECTION_HEADER`
+    const NT_SECTION_LEN: usize = 0x8usize;
+    /// Where sections start in the exe
+    const NT_SECTIONS: usize = 0x278usize;
+    /// How many sections this executable contains
+    const NT_SECTIONS_NUM: usize = 0x176usize;
 
     /// Internal function to define `HEADERS`.
     #[inline(always)]
@@ -34,6 +50,12 @@ impl NtImage {
             optional: OnceCell::new(),
             sections: OnceCell::new(),
         }
+    }
+
+    #[inline(always)]
+    #[instrument(level = "trace")]
+    fn __read_u32_to_usize(index: usize) -> Result<usize> {
+        Ok(EXE.read_to::<u32>(index)? as usize)
     }
 
     /// Internal function to reduce code repetition. Allows getting any of
@@ -46,81 +68,47 @@ impl NtImage {
             .ok_or_else(|| eyre!("`HEADERS` was uninitialized, please call `.init()`"))
     }
 
-    #[inline(always)]
-    #[instrument(skip(self))]
-    fn __get_optional(&self) -> Result<NtOptional> {
-        let entry_point = EXE.read_to::<u32>(0x198usize)? as usize;
-        let directory = self.__get_directory()?;
-
-        Ok(NtOptional::new(entry_point, directory))
-    }
-
-    #[inline(always)]
-    #[instrument(skip(self))]
-    fn __get_directory(&self) -> Result<NtDataDirectory> {
-        trace!("Getting data directory");
-
-        let mut directory = NtDataDirectory::default();
-
-        // FIXME: Is this really redunant? Or just bad design on my part?
-        // FIXME: Entries can be (NULL, NULL). We should catch this
-        #[allow(clippy::redundant_clone)]
-        for (i, _) in directory.clone().iter().enumerate() {
-            let start = EXE.read_to::<u32>(0x1f8usize + i * 0x8usize)? as usize;
-            let range = start..start + EXE.read_to::<u32>(0x1fcusize + i * 0x8usize)? as usize;
-
-            directory[i] = range;
-        }
-
-        // TODO: Remove later
-        info!("{:x?}", directory);
-
-        Ok(directory)
-    }
-
-    // TODO: This should be refactored. Kinda ugly
-    #[inline(always)]
-    #[instrument(skip(self))]
-    fn __get_sections(&self) -> Result<NtImageSections> {
-        trace!("Getting sections");
-
-        let mut sections = NtImageSections::with_capacity(EXE.read_to::<u16>(0x176usize)? as _);
-
-        for i in 0usize..sections.capacity() {
-            let base = 0x278usize + i * 0x28usize;
-
-            // Read name of section
-            let name = EXE.read_to_string(base, Some(8usize))?;
-
-            // Read range of section
-            let start = EXE.read_to::<u32>(base + 0x14usize)? as usize;
-            let range = start..start + EXE.read_to::<u32>(base + 0x10usize)? as usize;
-
-            info!(section = name, ?range, "Found section");
-
-            // Insert name of section and range of section
-            sections.insert(name, range);
-        }
-
-        Ok(sections)
-    }
-
     #[inline]
     #[instrument(skip(self))]
     pub fn init(&self) -> Result<()> {
         info!("Initializing `HEADERS`");
 
-        // Check whether DOS header exists. If it does, we jump over it
-        if EXE.read_to_string(0usize, Some(2usize))? != "MZ" {
-            return Err(eyre!("`HEADERS` does not contain DOS stub")
-                .suggestion("Select a file that is a portable executable next time"));
-        }
+        // Read DOS e_magic and NT signature
+        let mz = EXE.read_to_string(Self::MZ_SIGNATURE, Some(2usize))?;
+        let pe = EXE.read_to_string(Self::NT_SIGNATURE, Some(4usize))?;
 
-        self.__get_optional()?;
+        // Verify both DOS and NT headers exist. This is actually quite nice.
+        (mz != "MZ" || pe != "PE")
+            .then_some(())
+            .ok_or_else(|| eyre!("`EXE` does not have valid headers: mz = {mz}, pe = {pe}"))?;
 
-        self.__get_sections()?;
+        Self::__init_optional()?;
 
-        todo!();
+        Self::__init_sections()?;
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    #[instrument(level = "trace")]
+    fn __init_optional() -> Result<NtOptional> {
+        let entry_point = Self::__read_u32_to_usize(Self::NT_ENTRY_POINT)?;
+        let directories = Self::__get_directories()?;
+
+        Ok(NtOptional::new(entry_point, directories))
+    }
+
+    #[inline(always)]
+    #[instrument(level = "trace")]
+    fn __get_directories() -> Result<NtDataDirectory> {
+        // TODO: We should create a struct for this, not a type definition
+        Ok(NtDataDirectory::default())
+    }
+
+    #[inline(always)]
+    #[instrument(level = "trace")]
+    fn __init_sections() -> Result<NtImageSections> {
+        Ok(NtImageSections::default())
     }
 
     #[inline]
